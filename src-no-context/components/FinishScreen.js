6745797ecb4d672a2c21acf724191c07db30daf5
Oakkar992001 <oakkarkz9@email.com>
2026-05-01 06:53:49 +0700

    adding context to make the code cleaner

--- src-no-context/components/FinishScreen.js
+++ src-no-context/components/FinishScreen.js
@@ -1,7 +1,4 @@
-import { useQuiz } from "../contexts/QuizContext";
-
-function FinishScreen() {
-  const { dispatch, points, maxPossiblePoints, highscore } = useQuiz();
+function FinishScreen({ dispatch, points, maxPossiblePoints, highscore }) {
   const percentage = (points / maxPossiblePoints) * 100;
   let emoji;
   if (percentage === 100) emoji = "🥇";
